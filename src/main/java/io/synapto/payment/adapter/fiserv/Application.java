4feefffbe19c7b2d0dbcc09a9c06e303f23d8cd5
Iulian Postaru <postaru@gmail.com>
2024-08-21 16:33:15 +0300

    Change banner and namespaces.

--- src/main/java/io/synapto/payment/adapter/fiserv/Application.java
+++ src/main/java/io/synapto/payment/adapter/fiserv/Application.java
@@ -1,4 +1,4 @@
-package io.synapto.onboarding.adapter.fiserv;
+package io.synapto.payment.adapter.fiserv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
